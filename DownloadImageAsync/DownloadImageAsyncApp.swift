65c9cbaef806f68d3441a340c978abbc7012759f
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-22 12:21:39 +0200

    Initial Commit

---
+++ DownloadImageAsync/DownloadImageAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadImageAsyncApp.swift
+//  DownloadImageAsync
+//
+//  Created by Skorobogatow, Christian on 22/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadImageAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
